e35b21fd2ae3275c373b3290414eb947710e381e
Glacoon <guillian.celle@gmail.com>
2022-12-15 10:26:43 +0100

    fin des livres ingrédients et recettes

--- src/Classes/PHP/Recette.php
+++ src/Classes/PHP/Recette.php
@@ -14,10 +14,10 @@ class Ingredient
      */
 
     //ATTRIBUTS
-    public int $idIngredient;
-    public string $nomIngredient;
-    public float $prix;
-    public string $unite;
+    private int $idIngredient;
+    private string $nomIngredient;
+    private float $prix;
+    private string $unite;
 
     //CONSTRUCTEUR
 
@@ -33,7 +33,7 @@ class Ingredient
      * @function getUnite
      * @description Constructeurs de la classe Ingredient
      */
-    function __construct($idIngredient, $nomIngredient, $prix, $unite)
+    public function __construct($idIngredient, $nomIngredient, $prix, $unite)
     {
         $this->idIngredient = $idIngredient;
         $this->nomIngredient = $nomIngredient;
@@ -41,42 +41,42 @@ class Ingredient
         $this->unite = $unite;
     }
 
-    function setIdIngredient($idIngredient): void
+    public function setIdIngredient($idIngredient): void
     {
         $this->idIngredient = $idIngredient;
     }
 
-    function getIdIngredient(): int
+    public function getIdIngredient(): int
     {
         return $this->idIngredient;
     }
 
-    function setNomIngredient($nomIngredient): void
+    public function setNomIngredient($nomIngredient): void
     {
         $this->nomIngredient = $nomIngredient;
     }
 
-    function getNomIngredient(): string
+    public function getNomIngredient(): string
     {
         return $this->nomIngredient;
     }
 
-    function setPrix($prix): void
+    public function setPrix($prix): void
     {
         $this->prix = $prix;
     }
 
-    function getPrix(): float
+    public function getPrix(): float
     {
         return $this->prix;
     }
 
-    function setUnite($unite): void
+    public function setUnite($unite): void
     {
         $this->unite = $unite;
     }
 
-    function getUnite(): string
+    public function getUnite(): string
     {
         return $this->unite;
     }
@@ -88,7 +88,7 @@ class Ingredient
      * @description Permet d'afficher les informations de l'ingrédient
      * @return string
      */
-    function toString(): string
+    public function toString(): string
     {
         return "nomIngredient= " . $this->nomIngredient . ", prix= " . $this->prix . ", unite= " . $this->unite . " ";
     }
@@ -101,11 +101,11 @@ class Recette
      */
 
     //ATTRIBUTS
-    public int $idRecette;
-    public string $nomRecette;
-    public float $prixAjout;
-    public float $prixSacADos;
-    public array $ingredients = array();
+    private int $idRecette;
+    private string $nomRecette;
+    private float $prixAjout;
+    private float $prixSacADos;
+    private array $ingredients = array();
 
     //CONSTRUCTEUR
 
@@ -120,7 +120,7 @@ class Recette
      * @function getPrixAjout
      * @function getPrixSacADos
      */
-    function  __construct($idRecette, $nomRecette, $prixAjout, $prixSacADos)
+    public function  __construct($idRecette, $nomRecette, $prixAjout, $prixSacADos)
     {
         $this->idRecette = $idRecette;
         $this->nomRecette = $nomRecette;
@@ -128,7 +128,7 @@ class Recette
         $this->prixSacADos = $prixSacADos;
     }
 
-    function setIdRecette($idRecette): void
+    public function setIdRecette($idRecette): void
     {
         $this->idRecette = $idRecette;
     }
@@ -136,37 +136,37 @@ class Recette
         return $this->ingredients;
     }
 
-    function getIdRecette(): int
+    public function getIdRecette(): int
     {
         return $this->idRecette;
     }
 
-    function setNomRecette($nomRecette): void
+    public function setNomRecette($nomRecette): void
     {
         $this->nomRecette = $nomRecette;
     }
 
-    function getNomRecette(): string
+    public function getNomRecette(): string
     {
         return $this->nomRecette;
     }
 
-    function setPrixAjout($prixAjout): void
+    public function setPrixAjout($prixAjout): void
     {
         $this->prixAjout = $prixAjout;
     }
 
-    function getPrixAjout(): float|int
+    public function getPrixAjout(): float|int
     {
         return $this->prixAjout;
     }
 
-    function setPrixSacADos($prixSacADos): void
+    public function setPrixSacADos($prixSacADos): void
     {
         $this->prixSacADos = $prixSacADos;
     }
 
-    function getPrixSacADos(): float|int
+    public function getPrixSacADos(): float|int
     {
         return $this->prixSacADos;
     }
@@ -181,7 +181,7 @@ class Recette
      * @function ajouteIngredient
      * @description Permet d'ajouter un ingrédient à la recette
      */
-    function toString(): string
+    public function toString(): string
     {
         $str = "";
         foreach ($this->ingredients as $ingredient) {
@@ -199,12 +199,20 @@ $recette = new Recette(1, "Pain", 0.5, 0.5);
 $recette->ajouteIngredient(new Ingredient(1, "Farine", 0.5, "kg"));
 $recette->ajouteIngredient(new Ingredient(2, "Eau", 0.5, "L"));
 $recette->ajouteIngredient(new Ingredient(3, "Sel", 0.5, "kg"));
+$recette2 = new Recette(2, "pizza", 0.5, 0.5);
+$recette2->ajouteIngredient(new Ingredient(1, "Farine", 0.5, "kg"));
+$recette2->ajouteIngredient(new Ingredient(2, "Eau", 0.5, "L"));
+$recette2->ajouteIngredient(new Ingredient(3, "Sel", 0.5, "kg"));
+$recette2->ajouteIngredient(new Ingredient(4, "Oeuf", 0.5, "kg"));
+$recette2->ajouteIngredient(new Ingredient(5, "Tomate", 0.5, "kg"));
+$recette2->ajouteIngredient(new Ingredient(6, "Fromage", 0.5, "kg"));
+$recette2->ajouteIngredient(new Ingredient(7, "Jambon", 0.5, "kg"));
+$recette2->ajouteIngredient(new Ingredient(8, "Oignon", 0.5, "kg"));
+$recette2->ajouteIngredient(new Ingredient(9, "Olives", 0.5, "kg"));
 
 // get the list of ingredients in the recipe
 $ingredients = $recette->getIngredients();
-echo $recette->toString();
-
-$Pain = new Recette(1, "Pain", 0.5, 0.5 );
+$ingredients2 = $recette2->getIngredients();
 
 class livreRecette
 {
@@ -221,11 +229,14 @@ class livreRecette
      * @function ajouteRecette
      * @description Permet d'ajouter une recette au livre de recette
      */
-
     public function ajouteRecette(Recette $recette):void{
         $this->listeRecettes[] = $recette;
     }
 
+    /**
+     * @function supprRecette
+     * @description Permet de supprimer une recette du livre de recette
+     */
     public function supprRecette(Recette $recette):void{
         $cle = array_search($recette, $this->listeRecettes);
         if($cle !== false){
@@ -233,16 +244,25 @@ class livreRecette
         }
     }
 
+    /**
+     * @function getRecettes
+     * @description Permet d'afficher les recettes du livre de recette
+     */
     public function getRecettes(): array{
         return $this->listeRecettes;
     }
 }
 
+echo "<br>";
 $livreRecette = new livreRecette();
 $livreRecette->ajouteRecette($recette);
+$livreRecette->ajouteRecette($recette2);
 $Recettes = $livreRecette->getRecettes();
 
-echo $Recettes[0] ->toString();
+foreach ($Recettes as $recette) {
+    echo $recette->toString();
+    echo '<br>';
+}
 
 class livreIngredient
 {
@@ -253,5 +273,87 @@ class livreIngredient
     //ATTRIBUTS
     public array $listeIngredients = array();
 
+    //CONSTRUCTEUR
+
+    /**
+     * @function setListeIngredients
+     * @function getListeIngredients
+     * @function __construct
+     * @description Permet de créer un livre d'ingrédient avec ses ingrédients
+     */
+
+    public function setListeIngredients($listeIngredients): void
+    {
+        $this->listeIngredients = $listeIngredients;
+    }
+    public function __construct($listeIngredients)
+    {
+        $this->setListeIngredients($listeIngredients);
+    }
+
+    public function getListeIngredients(): array{
+        return $this->listeIngredients;
+    }
+
+    //MÉTHODES USUELLES
+
+    /**
+     * @function ajouteIngredient
+     * @description Permet d'ajouter un ingrédient au livre d'ingrédient
+     */
+    public function ajouteIngredient(Ingredient $ingredient):void{
+        $this->listeIngredients[] = $ingredient;
+    }
+
+    /**
+     * @function supprIngredient
+     * @description Permet de supprimer un ingrédient du livre d'ingrédient
+     */
+    public function supprIngredient(Ingredient $ingredient):void{
+        $cle = array_search($ingredient, $this->listeIngredients);
+        if($cle !== false){
+            unset($this->listeIngredients[$cle]);
+        }
+    }
+
+    /**
+     * @function modifIngredient
+     * @description Permet de modifier un ingrédient du livre d'ingrédient
+     */
+
+    public function modifIngredient(Ingredient $ingredient, Ingredient $newIngredient):void
+    {
+        $cle = array_search($ingredient, $this->listeIngredients);
+        if($cle !== false){
+            $this->listeIngredients[$cle] = $newIngredient;
+        }
+    }
+
+    /**
+     * @function getIngredients
+     * @description Permet d'afficher les ingrédients du livre d'ingrédient
+     */
+    public function getIngredients(): array
+    {
+        return $this->listeIngredients;
+    }
 
 }
+echo "<br>";
+$livreIngredient = new livreIngredient(array());
+
+foreach ($livreIngredient as $ingredient) {
+    foreach ($ingredient as $ingredient2) {
+        $livreIngredient->ajouteIngredient($ingredient2);
+    }
+}
+
+$MesIngredients = $livreIngredient->getIngredients();
+
+foreach ($MesIngredients as $ingredient) {
+    echo $ingredient->toString() . "<br>";
+}
+
+
+
+
