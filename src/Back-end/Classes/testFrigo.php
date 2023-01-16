1237abd647a4fc53195b44d76d7a46b79783257e
Angel0503 <angel.ag64100@gmail.com>
2023-01-16 10:19:33 +0100

    Frigo et son test finis

---
+++ src/Back-end/Classes/testFrigo.php
@@ -0,0 +1,41 @@
+<?php
+require "Frigo.php";
+require "Recette.php";
+require "Ingredient.php";
+
+$Ingredient1 = new Ingredient(1, "Oeuf", 0.15, "unite");
+$Ingredient2 = new Ingredient(2, "Lait", 1.04, "litre");
+$Ingredient3 = new Ingredient(3, "Salade", 2, "unite");
+$Ingredient4 = new Ingredient(4, "Chorizo", 5, "kg");
+$Ingredient5 = new Ingredient(5, "Feta", 9.80, "kg");
+$Ingredient6 = new Ingredient(6, "Thon", 17.83, "kg");
+$Ingredient7 = new Ingredient(7, "Mayonnaise", 6.99, "kg");
+$Ingredient8 = new Ingredient(8, "Riz", 1.96, "kg");
+
+$listeIngredients = array($Ingredient1, $Ingredient2, $Ingredient3);
+$listeQuantites = array(2, 1, 1);
+
+$Frigo = new Frigo($listeIngredients, $listeQuantites);
+$Frigo->ajouterIngredient($Ingredient4, 3);
+foreach ($Frigo->getIngredients() as $ingredient) {
+    echo $ingredient->toString() . "<br>";
+}
+
+echo "<BR>";
+
+foreach ($Frigo->getQuantites() as $quantite) {
+    echo $quantite . "<br>";
+}
+
+echo "<BR>";
+
+echo $Frigo->toString();
+
+echo "<BR>";
+
+echo $Frigo->getPrixFrigo();
+
+
+
+
+
--- src/Back-end/Main.php
+++ src/Back-end/Main.php
@@ -7,10 +7,11 @@
  */
 
 //INCLUSIONS
-include_once 'Ingredient.php';
-include_once 'Recette.php';
-include_once 'LivreIngredient.php';
-include_once 'LivreRecette.php';
+include_once 'Classes/Ingredient.php';
+include_once 'Classes/Recette.php';
+include_once 'Classes/LivreIngredient.php';
+include_once 'Classes/LivreRecette.php';
+include_once  'Classes/Frigo.php';
 
 $recette = new Recette(1, "Pain", 0.5, 0.5);
 $recette->ajouteIngredient(new Ingredient(1, "Farine", 0.5, "kg"));
