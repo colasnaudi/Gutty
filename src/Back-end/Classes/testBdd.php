3902d82a6afb4945f7f5f074e51ef787973afbab
Angel0503 <angel.ag64100@gmail.com>
2023-02-06 15:08:36 +0100

    Injection SQL impossible + bannissement utilisateur

--- src/Back-end/Classes/testBdd.php
+++ src/Back-end/Classes/testBdd.php
@@ -3,11 +3,9 @@ include 'BaseDeDonnees.php';
 
 $bddGutty = new BaseDeDonnees();
 
-$pseudo = "MatHis";
-$mail = "agarcia026@iutbayonne.univ-pau.fr";
+$pseudo = "userTest";
+$mail = "test@iutbayonne.univ-pau.fr";
 $mdp = "root";
 $mdp2 = "root";
 
-echo $bddGutty->checkConnexion($pseudo, $mdp);
-
-
+$bddGutty->bannirUtilsateur($pseudo);
