e2a8257dd92228816fd222756df8a182caa77754
Angel0503 <angel.ag64100@gmail.com>
2023-02-08 14:51:50 +0100

    Recherche par ingredient sans quantité géré

---
+++ src/Back-end/resultatRechercheIngredientSansQuantite.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Front-end/rechercheParIngredient.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <title>Accueil-Gutty</title>
+</head>
+<body>
+<header>
+    <img src="../Front-end/logo.png" alt="Logo temporaire">
+    <h1>Gutty</h1>
+</header>
+
+<div class="recherche">
+
+<?php
+include_once 'CreationIngredient.php';
+
+$ingredientFrigos = array();
+
+$livreIngredient = $_SESSION['livreIngredient'];
+$livreRecette = $_SESSION['livreRecette'];
+
+if(isset($_REQUEST['Ingredient']) && !empty($_REQUEST['Ingredient'])) {
+    if (is_array($_REQUEST['Ingredient'])) {
+        $ingredients = $_REQUEST['Ingredient'];
+
+        foreach($ingredients as $ingredient){
+            $ingredientFrigos[] = $livreIngredient->retrouverIngredient($ingredient);
+        }
+        $frigo = new Frigo($ingredientFrigos, null);
+    }
+}
+else
+{
+    echo "aucun ingrédient récupéré";
+    exit();
+}
+
+//Test generer suggestion
+//Generation des recettes possibles
+$recettePossible = $frigo->genererPossibleRecette($livreRecette);
+
+//Calcul du nombre d'ingrédient en commun
+foreach ($recettePossible as $recette) {
+    $recette->compterIngredientCommun($frigo);
+}
+
+//Tri des recettes possibles par PourcentageFrigo
+$recetteTriee = $frigo->trierSuggestionSansQuantite($recettePossible);
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Front-end/rechercheParIngredient.css">
+    <link rel="stylesheet" href="../Front-end/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <title>Accueil-Gutty</title>
+</head>
+<body>
+<header>
+    <img src="../Front-end/logo.png" alt="Logo temporaire">
+    <h1>Gutty</h1>
+</header>
+<div class="recipes-container">
+    <h2>Liste de recettes</h2>
+    <ul>
+        <?php for($i=0; $i<sizeof($recetteTriee); $i++){ ?>
+            <li class="recipe">
+                <a href="afficherUneRecette.php?recette=<?php echo $recetteTriee[$i]->getNomRecette(); ?>">
+                    <?php
+                    echo $recetteTriee[$i]->getNomRecette() . " : " . $recetteTriee[$i]->getPrixRecette() . "€"
+                        . "Ingrédients utilisés:" . $recetteTriee[$i]->getNbIngredientCommun() . "/" . count($frigo->getIngredients())?>
+            </li>
+        <?php } ?>
+    </ul>
+</div>
+</body>
+</html>
