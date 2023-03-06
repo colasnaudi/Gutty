f3e3885ec32ba5008864849c3dad1854ea0c286a
Angel0503 <angel.ag64100@gmail.com>
2023-03-06 15:28:44 +0100

    Ajout bandeau aux endroits fini

--- src/Front-end/afficherUneRecette.php
+++ src/Front-end/afficherUneRecette.php
@@ -11,43 +11,8 @@
     <title>Affichage recette-Gutty</title>
 </head>
 <body>
-<header>
-    <div class="partieHaute">
-        <div class="logoEtTitre">
-            <img src="logo.png" alt="Logo temporaire">
-            <h1>Gutty</h1>
-        </div>
-        <div class="recherche">
-            <input type="text" name="" id="ing_recherche" placeholder="Je recherche" />
-            <a class="bouton_recherche" href="#">
-                <i class="material-icons iconeRecherche">search</i>
-            </a>
-        </div>
-        <div class="monCompte">
-            <i class="material-icons iconeCompte">person</i>
-        </div>
-    </div>
-    <div class="partieBasse">
-        <div class="rechercheIngredient">
-            <button>Recherche par ingrédient</button>
-        </div>
-        <div class="ajouterRecette">
-            <button>Ajouter une recette</button>
-        </div>
-        <div class="recettesFavorites">
-            <button>Recettes favorites</button>
-        </div>
-
-
-    </div>
-</header>
-<main>
-
-
-</main>
-
-
 <?php
+include_once 'header.html';
 
 //INCLUSIONS
 
