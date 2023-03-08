97ebf31945b16985ca3b28b6284a30e44a41b07b
Glacoon <guillian.celle@gmail.com>
2023-03-08 10:35:20 +0100

    fin du CSS de la recherche et des recettes

--- src/Back-end/resultatRechercheIngredientSansQuantite.php
+++ src/Back-end/resultatRechercheIngredientSansQuantite.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Front-end/resultatQuantiteRecette.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-    <title>Accueil-Gutty</title>
-</head>
-<body>
 <?php
-
-include_once "../Front-end/header.html";
-
 include_once 'CreationIngredient.php';
 
 $ingredientFrigos = array();
@@ -50,18 +36,47 @@ foreach ($recettePossible as $recette) {
 $recetteTriee = $frigo->trierSuggestionSansQuantite($recettePossible);
 ?>
 
-<div class="recipes-container">
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Front-end/bootstrap-3.3.7-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../Front-end/resultatQuantiteRecette.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <title>Accueil-Gutty</title>
+</head>
+<body>
+<?php
+    include_once "../Front-end/header.html";
+?>
+<div class="container recipes-container">
     <h2>Liste de recettes <?php echo "(".sizeof($recetteTriee)." recettes)"  ?></h2>
-    <ul>
-        <?php for($i=0; $i<sizeof($recetteTriee); $i++){ ?>
-            <li class="recipe">
-                <a href="afficherUneRecette.php?recette=<?php echo $recetteTriee[$i]->getNomRecette(); ?>">
-                    <?php
-                    echo $recetteTriee[$i]->getNomRecette() . " : " . $recetteTriee[$i]->getPrixRecette() . "€ "
-                        . "Ingrédients utilisés : " . $recetteTriee[$i]->getNbIngredientCommun() . "/" . count($frigo->getIngredients())?>
-            </li>
-        <?php } ?>
-    </ul>
+    <div class="row">
+        <ul>
+            <?php for($i=0; $i<sizeof($recetteTriee); $i++){ ?>
+            <a href="../Front-end/afficherUneRecette.php?recette=<?php echo $recetteTriee[$i]->getNomRecette(); ?>" class="col-lg-4">
+                <li class="recipe">
+                        <img src="../Front-end/raclette.png" id="menu">
+                        <br>
+                        <div class="info">
+                            <div class="name">
+                                <?php
+                                echo $recetteTriee[$i]->getNomRecette(). " ";
+                                echo $recetteTriee[$i]->getPrixRecette() . "€ ";
+                                ?>
+                            </div>
+                        <?php
+                        echo "<br>";
+                        echo "Ingrédients utilisés : " . $recetteTriee[$i]->getNbIngredientCommun() . "/" . count($frigo->getIngredients())?>
+                        </div>
+                    <img src="../Front-end/logo.png" id="user">
+                </li>
+            </a>
+            <?php } ?>
+        </ul>
+    </div>
 </div>
 </body>
 </html>
