f31f7425ee012474a4e43fd7f3178dc1b59ab63d
Angel0503 <angel.ag64100@gmail.com>
2023-03-09 11:44:56 +0100

    Recherche par nom et header réparé

--- src/Back-end/rechercheParNom.php
+++ src/Back-end/rechercheParNom.php
@@ -8,37 +8,9 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <title>Accueil-Gutty</title>
 </head>
-<body>
-<header>
-    <div class="partieHaute">
-        <div class="logoEtTitre">
-            <img src="../Front-end/logo.png" alt="Logo temporaire">
-            <h1>Gutty</h1>
-        </div>
-        <div class="recherche">
-            <input type="text" name="recetteRecherche" id="ing_recherche" placeholder="Je recherche" />
-            <a class="bouton_recherche" href="rechercheParNom.php">
-                <i class="material-icons iconeRecherche">search</i>
-            </a>
-        </div>
-        <div class="monCompte">
-            <i class="material-icons iconeCompte">person</i>
-        </div>
-    </div>
-    <div class="partieBasse">
-        <div class="rechercheIngredient">
-            <button onclick="location.href='rechercheParIngredient.php'">Recherche par ingrédient</button>
-        </div>
-        <div class="ajouterRecette">
-            <button>Ajouter une recette</button>
-        </div>
-        <div class="recettesFavorites">
-            <button>Recettes favorites</button>
-        </div>
-    </div>
-</header>
 
 <?php
+include_once '../Front-end/header.html';
 include 'Classes/BaseDeDonnees.php';
 $bdd = new BaseDeDonnees();
 
